ef853aa9e19ae88e98d42be4201c6d528fecf5c3
Panagiotis Papadomitsos <pj@bugsense.com>
2014-01-24 15:24:18 +0200

    Updated download to :create instead of :create_if_missing

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,13 +3,13 @@
 # Recipe:: default
 #
 # Copyright (C) 2013 Panagiotis Papadomitsos
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #    http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -27,7 +27,7 @@ remote_file "#{Chef::Config['file_cache_path'] || '/tmp'}/#{jem_filename}" do
   mode 00644
   source node['jemalloc']['url']
   checksum node['jemalloc']['checksum']
-  action :create_if_missing
+  action :create
 end
 
 execute 'extract-jemalloc' do
